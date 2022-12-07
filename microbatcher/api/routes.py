284d5eb12f60931f36e54d1977f7311bf450cdb8
Biswaroop Bhattacharjee <biswaroop08@gmail.com>
2022-12-07 20:08:08 +0530

    update: more stable internal functions with timeouts, flexibility and asyncio

--- microbatcher/api/routes.py
+++ microbatcher/api/routes.py
@@ -1,7 +1,9 @@
 import asyncio
+import traceback
 from time import sleep, time
 
 from fastapi.concurrency import run_in_threadpool
+from loguru import logger
 
 import microbatcher.constants as const
 from microbatcher.api import app, loaders
@@ -14,28 +16,44 @@ loadedModel = loaders.Model()
 global_queue.set_model(loadedModel)
 
 
-async def get_response(queue_, datapoint):
-    while not (pred := queue_.get_prediction(datapoint.uuid)):
-        await asyncio.sleep(0.0000001)
-    return pred
+async def get_response(queue, datapoint):
+    # Wait for the prediction to be available
+    while True:
+        try:
+            if pred := await asyncio.wait_for(
+                queue.get_prediction(datapoint.uuid), timeout=const.OPS_TIMEOUT
+            ):
+                return pred
+        except asyncio.TimeoutError as e:
+            logger.error(f"Timeout happened while trying to fetch response\n{e}")
 
 
-async def enqueue_request(queue_, datapoint):
-    while global_queue.put(datapoint) is False:
-        await asyncio.sleep(0.0000001)
+async def enqueue_request(queue, datapoint):
+    # Wait for the queue to be ready to accept items
+    while True:
+        try:
+            if await asyncio.wait_for(queue.put(datapoint), timeout=const.OPS_TIMEOUT):
+                return
+        except asyncio.TimeoutError as e:
+            logger.error(f"Timeout happened while enqueuing request\n{e}")
 
 
-async def queue_patience_timeout(queue_):
-    while (
-        queue_.start
-        and ((time() - queue_.start) < const.QUEUE_TIMEOUT)
-        and (queue_.item_count <= const.QUEUE_MAXSIZE)
-    ):
-        await asyncio.sleep(0.0000001)
+async def queue_patience_timeout(queue):
+    # Wait for the queue to fill up/timeout to start processing
+    while True:
+        try:
+            if await asyncio.wait_for(
+                queue.patience_timeout(), timeout=const.OPS_TIMEOUT
+            ):
+                return
+        except asyncio.TimeoutError as e:
+            logger.error(
+                f"Timeout happened while checking if queue is filled up\n{traceback.format_exc()}"
+            )
 
 
 @app.post("/predict/")
-async def read_root(payload: requests.Input):
+async def predict(payload: requests.Input):
     datapoint = DataPoint(payload.dict().get("data"))
     await enqueue_request(global_queue, datapoint)
     await queue_patience_timeout(global_queue)
--- microbatcher/types/queues.py
+++ microbatcher/types/queues.py
@@ -1,9 +1,12 @@
+import asyncio
 import multiprocessing
 import threading
 from multiprocessing.queues import Queue
-from time import time
+from time import sleep, time
 
 import numpy as np
+
+import microbatcher.constants as const
 
 
 class MicroQueue(Queue):
@@ -20,9 +23,10 @@ class MicroQueue(Queue):
 
 
 class ProcessorQueue:
-    def __init__(self, maxsize=999999999):
+    def __init__(self, maxsize=const.QUEUE_MAXSIZE, timeout=const.QUEUE_TIMEOUT):
         self.queue = MicroQueue()
         self.max_queue_size = maxsize
+        self.timeout = timeout
         self.mutex = threading.Lock()
         self.tasks_processing = threading.Condition(self.mutex)
         self.task_enqueue = threading.Condition(self.mutex)
@@ -33,24 +37,30 @@ class ProcessorQueue:
         self.item_count = 0
         self.is_done = {}
 
-    def put(self, obj):
-        # self.mutex.acquire()
-        with self.mutex:
-            queue_space_available = False
-            try:
-                if self.item_count < self.max_queue_size:
-                    self.queue.put(obj)
-                    self.item_count += 1
-                    print(f"item count increased: {self.item_count}")
-                    self.start_time()
-                    queue_space_available = True
-            finally:
-                # self.mutex.release()
-                return queue_space_available
+    async def patience_timeout(self):
+        return (
+            not self.start
+            or ((time() - self.start) >= self.timeout)
+            or (self.item_count > self.max_queue_size)
+        )
+
+    async def put(self, obj):
+        queue_space_available = False
+        try:
+            self.mutex.acquire()
+
+            if self.item_count < self.max_queue_size:
+                self.queue.put_nowait(obj)
+                self.item_count += 1
+                print(f"item count: {self.item_count}")
+                self.start_time()
+                queue_space_available = True
+        finally:
+            self.mutex.release()
+        return queue_space_available
 
     def start_time(self):
         if self.item_count == 1:
-            print("time initialized")
             self.start = time()
 
     def reset_processed(self):
@@ -75,31 +85,23 @@ class ProcessorQueue:
     def all_processes_done(self):
         return not self.is_done or all(list(self.is_done.values()))
 
-    def get_prediction(self, uuid):
-        # print("processed dict", self.processed_outputs)
-        # print("results,", self.results)
+    async def get_prediction(self, uuid):
         pred = None
-        # self.mutex.acquire()
-        with self.mutex:
-            try:
-                if pred := self.processed_outputs.get(uuid):
-                    self.is_done[uuid] = True
-                    print("pred foundddd:", pred)
-            finally:
-                # self.mutex.release()
-                return pred
+        try:
+            if pred := self.processed_outputs.get(uuid):
+                self.is_done[uuid] = True
+        finally:
+            return pred
 
     def process_items(self):
         run_success = False
         with self.mutex:
-            # acquire lock
             try:
                 if (
                     (model := self.model)
                     and not self.queue.empty()
                     and self.all_processes_done()
                 ):
-                    print("inside processing")
                     # process items in queue
                     datapoints = []
                     uuids = []
@@ -107,8 +109,8 @@ class ProcessorQueue:
                     for idx, datapoint in enumerate(self.queue):
                         datapoints.append(datapoint.data)
                         uuids.append(datapoint.uuid)
-                    print(f"processingggg {len(datapoints)} datapoints")
-                    self.results = model.predict(np.array(datapoints))
+
+                    self.results = model.predict(np.array(datapoints, dtype=object))
                     self.processed_outputs = {
                         uuids[idx]: res.tolist()
                         for idx, res in enumerate(self.results)  # noqa: E501
@@ -119,6 +121,4 @@ class ProcessorQueue:
                     self.reset_time(datapoints_processed=len(self.results))
                     run_success = True
             finally:
-                # release lock
-                # self.mutex.release()
                 return run_success
